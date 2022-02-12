af2fdc2ecc1512dbcea61a00852ec5066c6cacb1
hujianglang <hujianglang@126.com>
2022-02-13 00:00:07 +0800

    example1

--- blockchain/solidity/test.sol
+++ blockchain/solidity/test.sol
@@ -1,5 +1,12 @@
-contract test{
-    function myFirst(address _MyAddress) public {
-        //Do
+// SPDX-License-Identifier:GPL-3.0
+pragma Solidity >= 0.7.0 <0.9.0;
+contract Storage{
+    uint 256 number;
+    function store(uint256 num) public {
+        number = num;
+    }
+
+    function retrieve() public view returns (unit256){
+        return number;
     }
 }
